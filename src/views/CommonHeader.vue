d816f1e09f23b82e40c602f48d50d643d68ce3f5
browonkim <browonkim@gmail.com>
2022-04-23 02:01:28 +0900

    create common layouts

---
+++ src/views/CommonHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-header">
+    <header><h1>Law&Good List</h1>
+      <slot></slot>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+.app-header
+  box-sizing: border-box
+  width: 100vw
+  height: 40px
+  padding-top: 8px
+  padding-left: 5px
+
+  border-bottom: 1px solid gray
+
+  header
+    h1
+      font-size: 25px
+</style>
---
+++ src/views/CommonLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="common-layout">
+    <CommonHeader/>
+    <router-view/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CommonHeader from "@/views/CommonHeader.vue"
+
+</script>
+
+<style scoped lang="sass">
+.common-layout
+  margin: 0
+  padding: 0
+</style>
