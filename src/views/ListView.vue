564c63bed301bf64dda4937fff320942cb46a755
browonkim <browonkim@gmail.com>
2022-04-23 02:01:48 +0900

    create list view

---
+++ src/views/ListView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="list-view">
+    <ItemCard/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ItemCard from '@/components/ItemCard.vue'
+</script>
+
+<style scoped>
+
+</style>
