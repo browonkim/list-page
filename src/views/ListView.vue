52980699a78264b2e9a8e79d61a63ec50f3ae1f8
browonkim <browonkim@gmail.com>
2022-04-23 17:04:44 +0900

    add list-modal in ListView.vue

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="list-view">
-    <ItemCard/>
+  <div>
+    <ItemModal/>
+    <div class="list-container">
+      <ItemCard v-for="item in listItems" :key="item.id" :listItem="item" class="list-item"/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ItemCard from '@/components/ItemCard.vue'
+import axios from "axios";
+import {onMounted, ref} from "vue";
+import {ListItem} from "@/types/common-types";
+import ItemModal from "@/components/ItemModal.vue";
+
+const listItems = ref<Array<ListItem>>()
+
+onMounted(() => {
+  axios.get('http://localhost:8080/api/list')
+      .then(response => {
+        const retrievedListItems = response.data?.list ?? []
+        listItems.value = [...retrievedListItems]
+      })
+})
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+.list-container
+  display: grid
+  width: 100vw
+  max-width: 100%
 
+  .list-item
+    margin: 6px 3px 0 6px
 </style>
