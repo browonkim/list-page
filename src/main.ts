06eeba6d3500d8cfdd1d94b95ebb123b3bc5e4be
browonkim <browonkim@gmail.com>
2022-04-23 17:05:36 +0900

    add temporary server (would be removed)

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {makeServer} from "@/mockup-server/mockup-server";
+
+makeServer()
 
 createApp(App).use(store).use(router).mount('#app')
+
