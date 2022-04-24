33360d9dbd7e90705c25138dd1f3a99db9e4b16d
browonkim <browonkim@gmail.com>
2022-04-24 14:36:34 +0900

    add script for initializing the local storage

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {makeServer} from "@/mockup-server/mockup-server";
+import {makeServer} from "@/mockup/mockup-server";
+import {mockData} from "@/mockup/mock-data";
 
+// mock server (should be deleted)
 makeServer()
+
+// local storage initializing
+if (window.localStorage.getItem('data') === null) {
+    window.localStorage.setItem('data', JSON.stringify(mockData))
+}
 
 createApp(App).use(store).use(router).mount('#app')
 
