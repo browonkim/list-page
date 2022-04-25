e18c931adb16eea9f1461b0b2028b45ca6e878bd
browonkim <browonkim@gmail.com>
2022-04-25 19:55:34 +0900

    remove code of initializing mockup-server

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {makeServer} from "@/mockup/mockup-server";
 import {mockData} from "@/mockup/mock-data";
-
-// mock server (should be deleted)
-makeServer()
 
 // local storage initializing
 if (window.localStorage.getItem('data') === null) {
